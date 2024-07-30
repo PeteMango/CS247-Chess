96eb167c50a7c0391a21014366e4cc594cef1d70
Norman Chen <njchen@uwaterloo.ca>
2024-07-30 19:02:20 -0400

    add pdfs

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -1,1 +1,2 @@
-zip -r chess.zip .github build include out scripts/test.sh src tests/in/valid tests/expect/valid tests/run_tests.sh run_bots.sh Makefile
+make clean
+zip -r chess.zip .github build include out scripts/test.sh src tests/in/valid tests/expect/valid tests/run_tests.sh run_bots.sh makefile uml.pdf design.pdf
